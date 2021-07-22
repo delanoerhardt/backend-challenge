5f6ade2451a43e0f5c5b1a8aeaf02cf295de29d0
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-07-22 02:16:16 -0300

    Changed database model

--- create_table.sql
+++ create_table.sql
@@ -1,16 +1,3 @@
-CREATE TABLE equipment_table
-(
- _id      uuid NOT NULL,
- tool     varchar(50) NOT NULL,
- CONSTRAINT PK_equipment_table PRIMARY KEY ( _id )
-);
-
-CREATE TABLE ingredient_table
-(
- _id      uuid NOT NULL,
- food     varchar(35) NOT NULL,
- CONSTRAINT PK_ingredient_table PRIMARY KEY ( _id )
-);
 
 CREATE TABLE recipe_table
 (
@@ -20,30 +7,29 @@ CREATE TABLE recipe_table
  CONSTRAINT PK_recipe_table PRIMARY KEY ( _id )
 );
 
-CREATE INDEX fkIdx_0 ON recipe_table
+CREATE TABLE ingredient_table
 (
- _id
+ _id      uuid NOT NULL,
+ food     varchar(35) NOT NULL,
+ CONSTRAINT PK_ingredient_table PRIMARY KEY ( _id )
+);
+
+CREATE TABLE equipment_table
+(
+ _id      uuid NOT NULL,
+ tool     varchar(50) NOT NULL,
+ CONSTRAINT PK_equipment_table PRIMARY KEY ( _id )
 );
 
 CREATE TABLE ingredient_of_recipe
 (
- recipe_id     uuid NOT NULL,
+ recipe_id     uuid NOT NULL, 
  ingredient_id uuid NOT NULL,
  quantity_unit varchar(10) NOT NULL,
  quantity      varchar(30) NOT NULL,
  CONSTRAINT PK_ingredient_of_recipe PRIMARY KEY ( recipe_id, ingredient_id ),
  CONSTRAINT FK_40 FOREIGN KEY ( recipe_id ) REFERENCES recipe_table ( _id ),
  CONSTRAINT FK_44 FOREIGN KEY ( ingredient_id ) REFERENCES ingredient_table ( _id )
-);
-
-CREATE INDEX fkIdx_3 ON ingredient_of_recipe
-(
- recipe_id
-);
-
-CREATE INDEX fkIdx_4 ON ingredient_of_recipe
-(
- ingredient_id
 );
 
 CREATE TABLE equipment_of_recipe
@@ -56,12 +42,7 @@ CREATE TABLE equipment_of_recipe
  CONSTRAINT FK_56 FOREIGN KEY ( recipe_id ) REFERENCES recipe_table ( _id )
 );
 
-CREATE INDEX fkIdx_1 ON equipment_of_recipe
+CREATE INDEX ingredient_id ON ingredient_of_recipe
 (
- equipment_id
-);
-
-CREATE INDEX fkIdx_2 ON equipment_of_recipe
-(
- recipe_id
+  ingredient_id
 );
