57cf677e92d56e89b5fc7d5f06e8f37901e07f9a
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-07-23 20:26:51 -0300

    Changed script for adding recipes

--- scripts/add_recipes.lua
+++ scripts/add_recipes.lua
@@ -2,22 +2,54 @@
 wrk.method = "POST"
 wrk.headers["Content-Type"] = "application/json"
 
-array = {}
-array_size = 10
+math.randomseed(os.time())
 
-init = function()
-  math.randomseed(os.time())
+base_ingredient = [[{"food":"%s","quantity":"9","quantity_unit":"ml"}]]
+base_equipment = [[{"tool":"%s","quantity":9}]]
+base_recipe = [[
+  {"name":"%sbread","description":"Lorem ipsum","ingredients":[%s],"equipments":[%s]}
+]]
 
-  local base = [[
-    {"name":"breada%d","equipments":[{"tool":"equipd%d","quantity":8}],"ingredients":[{"quantity_unit":"ml","quantity":"%d","food":"ingrea%d"},{"quantity_unit":"kg","quantity":"%d","food":"waterb%d"}],"description":"Yep, it's bread"}
-  ]]
-  for i = 1, array_size do
-    array[i] = string.format(base, math.random(1000000), math.random(1000000), math.random(1000000), math.random(1000000), math.random(1000000), math.random(1000000))
-    print(array[i])
-  end
-end
+food_examples = {"flour", "water", "yeast", "salt", "oil"}
+food_examples_amount = #food_examples
+
+tool_examples = {"bowl", "cup", "spoon", "spatula ", "oven"}
+tool_examples_amount = #tool_examples
+
+name_sufixes = {"Italian ", "Crumbs of ", "Banana ", "Flat", "Buckwheat "}
+name_sufixes_amount = #name_sufixes
+
+max_ingredients = 20
+max_equipments = 10
+
+arbitrary_value = 10000000
 
 request = function()
-  wrk.body = array[math.random(array_size)]
+  ingredients = {}
+  ingredient_amount = math.random(max_ingredients)
+
+  for i = 1, ingredient_amount do
+    ingredients[i] =
+      string.format(base_ingredient,
+          food_examples[math.random(food_examples_amount)]
+          .. math.random(arbitrary_value))
+  end
+
+  equipments = {}
+  equipments_amount = math.random(max_equipments)
+
+  for i = 1, equipments_amount do
+    equipments[i] =
+        string.format(base_equipment,
+          tool_examples[math.random(tool_examples_amount)]
+          .. math.random(arbitrary_value))
+  end
+
+  wrk.body =
+      string.format(base_recipe,
+        name_sufixes[math.random(name_sufixes_amount)],
+        table.concat(ingredients, ","),
+        table.concat(equipments, ",")
+      )
   return wrk.format("POST")
 end
