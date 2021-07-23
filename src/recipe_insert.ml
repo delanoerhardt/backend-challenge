c410e20aa146d2a2e373818380bf755906689952
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-07-23 20:27:52 -0300

    Insert now only returns after insertion

--- src/recipe_insert.ml
+++ src/recipe_insert.ml
@@ -1,6 +1,8 @@
 open Queries_insert
 open Types
 
+let ( let* ) = Lwt.bind
+
 let lwt_bind_unit a = Lwt.bind a (fun _ -> Lwt.return_unit)
 
 let get_ingredients_as_tuples recipe_id ingredients =
@@ -71,18 +73,15 @@ let add_recipe { name; description; ingredients; equipments } =
     ]
   in
 
-  let entities_inserts = Lwt.join lwt_list in
+  let* (_ : unit) = Lwt.join lwt_list in
 
-  let _connections_insert =
-    Lwt.map
-      (fun _ ->
-        ignore
-        @@ Database_handler.dispatch_query
-             (insert_ingredient_of_recipe_query ingred_of_recipe_list);
-        ignore
-        @@ Database_handler.dispatch_query
-             (insert_equipment_of_recipe_query equip_of_recipe_list))
-      entities_inserts
+  let* (_ : (unit, Caqti_error.t) result) =
+    Database_handler.dispatch_query
+      (insert_ingredient_of_recipe_query ingred_of_recipe_list)
+  in
+  let* (_ : (unit, Caqti_error.t) result) =
+    Database_handler.dispatch_query
+      (insert_equipment_of_recipe_query equip_of_recipe_list)
   in
 
   Lwt.return @@ Ok "Recipe added sucessfully"
