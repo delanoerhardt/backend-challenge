c46f2806cbb4935d640b3f05451375e4c541ffc0
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-07-23 02:10:30 -0300

    Refactored routes out of main file p.2

---
+++ src/routes.ml
@@ -0,0 +1,58 @@
+open Opium
+
+let ( let* ) = Lwt.bind
+
+let post_recipe request =
+  let* json_option = Request.to_json request in
+
+  let* add_recipe_result =
+    match json_option with
+    | None -> Lwt.return @@ Error "Invalid JSON"
+    | Some json -> (
+        match Recipe.recipe_of_yojson json with
+        | Error error -> Lwt.return @@ Error ("Malformed recipe in: " ^ error)
+        | Ok recipe -> Recipe_insert.add_recipe recipe)
+  in
+
+  match add_recipe_result with
+  | Error error ->
+      Lwt.return
+        (Response.make ~status:`Bad_request ~body:(Body.of_string error) ())
+  | Ok message ->
+      Lwt.return (Response.make ~status:`OK ~body:(Body.of_string message) ())
+
+let get_recipes_in_page request =
+  let start_time = Sys.time () in
+
+  let recipes_per_page = 8 in
+  let last_seen_id =
+    try Router.param request "last_seen_id"
+    with Not_found -> "00000000-0000-0000-0000-000000000000"
+  in
+
+  let* result = Recipe_read.get_recipes_in_page last_seen_id recipes_per_page in
+
+  print_endline @@ Printf.sprintf "%1.6F" (Sys.time () -. start_time);
+
+  match result with
+  | Error error ->
+      Response.of_plain_text ~status:`Bad_request error |> Lwt.return
+  | Ok result ->
+      Recipe.recipes_with_uuid_to_yojson result
+      |> Response.of_json |> Lwt.return
+
+let get_recipes_by_ingredient ingredient_id =
+  let* result = Recipe_read.get_recipes_from_ingredient ingredient_id in
+
+  match result with
+  | Error error ->
+      Response.of_plain_text ~status:`Bad_request error |> Lwt.return
+  | Ok result ->
+      Recipe.recipe_list_to_yojson result |> Response.of_json |> Lwt.return
+
+let get_recipes_by_ingredient_name request =
+  Router.param request "ingredient_name"
+  |> Uuid_handler.get_uuid_from_string |> get_recipes_by_ingredient
+
+let get_recipes_by_ingredient_id request =
+  Router.param request "ingredient_id" |> get_recipes_by_ingredient
