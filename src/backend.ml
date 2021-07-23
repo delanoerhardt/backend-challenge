f8a77a59b0f5ddf3dfba4882fb10ec802fa5eee4
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-07-23 20:24:10 -0300

    Changed route for getting recipes by ingredient

--- src/backend.ml
+++ src/backend.ml
@@ -8,7 +8,8 @@ let () =
   |> App.post "/add-recipe" post_recipe
   |> App.get "/get-recipes/" get_recipes_in_page
   |> App.get "/get-recipes/:last_seen_id" get_recipes_in_page
-  |> App.get "/get-recipes/by-name/:ingredient_name"
+  |> App.get "/recipes-with-ingredient/by-name/:ingredient_name"
        get_recipes_by_ingredient_name
-  |> App.get "/get-recipes/by-id/:ingredient_id" get_recipes_by_ingredient_id
+  |> App.get "/recipes-with-ingredient/by-id/:ingredient_id"
+       get_recipes_by_ingredient_id
   |> App.run_multicore
